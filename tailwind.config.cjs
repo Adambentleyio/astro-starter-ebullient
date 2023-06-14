bf4e44c93ced77bb1d09c8e42c99b75dc0cf84a3
Adam Bentley <61187053+Adambentleyio@users.noreply.github.com>
2023-06-14 15:34:18 +0100

    Style changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,css}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        sans: ["Parisienne", ...defaultTheme.fontFamily.sans],
       },
     },
   },
